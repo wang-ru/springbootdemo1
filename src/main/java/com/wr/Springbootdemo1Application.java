a2ab265637b08dee25dedcaabfb9d8f379ee989a
wangru <1349499641@qq.com>
2018-04-12 22:46:13 +0800

    asdfaf

--- src/main/java/com/wr/Springbootdemo1Application.java
+++ src/main/java/com/wr/Springbootdemo1Application.java
@@ -15,31 +15,37 @@ import java.util.Date;
 @SpringBootApplication
 public class Springbootdemo1Application {
 
-	@RequestMapping(value = "hello")
+    @RequestMapping(value = "hello")
     @ResponseBody
-	public String hello(@RequestParam(name = "xage",required = false) Integer age) {
-		return "age:"+age;
-	}
+    public String hello(@RequestParam(name = "xage", required = false) Integer age) {
+        return "age:" + age;
+    }
+//asdfadsfa
     @RequestMapping("aaa")
     @ResponseBody
+//    sfasdfasfdasdfafds
     public String hello1(User user, Admin admin) {
-        return "user:"+user.toString()+" admin"+admin.toString();
+        return "user:" + user.toString() + " admin" + admin.toString();
     }
+
     @InitBinder("user")
     public void initUser(WebDataBinder webDataBinder) {
         webDataBinder.setFieldDefaultPrefix("user.");
     }
+
     @InitBinder("admin")
     public void initAdmin(WebDataBinder webDataBinder) {
         webDataBinder.setFieldDefaultPrefix("admin.");
     }
+
     @RequestMapping("date")
     @ResponseBody
     public String date(Date date) {
         return date.toString();
     }
+
     public static void main(String[] args) {
-		SpringApplication.run(Springbootdemo1Application.class, args);
-	}
+        SpringApplication.run(Springbootdemo1Application.class, args);
+    }
 
 }
